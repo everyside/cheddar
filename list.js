e199ed74967198d89f09e6d30ff1db9ae6a87854
Dani Pletter <dani@fawnapp.com>
2015-06-30 20:08:07 -0700

    add some structure. wip.

--- list.js
+++ list.js
@@ -1,194 +1,179 @@
-var githubName = "Cheddar";
-var githubToken = null;
-var githubUser = null;
-var repo = null;
+var cheddar = (function CheddarController(){
 
-function initGithub(){
-  var ret = new Github({
-    token: githubToken,
-    auth: "oauth"
-  });
-  
-  var user = ret.getUser();
-  if(user){
-    console.log(user);
-    user.show(null,function(err, info) {
-      githubUser = info.login;
+  var ret = {
+    
+    user : {
+      github : {
+        name : "Cheddar",
+        token : null,
+        user : null,
+      }
+    },
+    
+    shapeViewControllers : {},
+    
+    init : function(callback){
+      chrome.storage.sync.get("githubToken", function(val){
+        this.user.github.token = val.githubToken;
       
-      updateRepoName();
-    });
-    return ret;
-  }else{
-    return null;
-  }
-}
-
-function generateName(){
-  var name = generate_name("shapes").toLowerCase();
-  $("#inputName").val(name);
-  updateRepoName();
-  return false;
-}
-
-function updateRepoName(){
-  chrome.storage.sync.get("repoPrefix", function(val){
-    var name = $("#inputName").val();
-    var prefix = val.repoPrefix || githubUser + "/cheddar-shape-";
-    var repoName = prefix + name;
-    $("#inputRepoName").val(repoName);
-  });
-}
-
-window.addEventListener("message", function(event){
-  //var repoName = "FIXME";FIXME -- need to pass the shape metadata around.
-  //save(repoName, event.data);
-});
-
-function save(repoName, code){
-  console.log("save!");
-  
-  var repo = getRepo(userName+"/"+shapeRepoName);
-  repo.readRef("refs/heads/"+branchName, function(err, headHash){
-      
-      var filename = shapeName + ".jscad";
-      var updates = {
-      };
-      updates[filename] = {mode:modes.file, content:code};
-      repo.createTree(updates, function(error, treeHash){
+        ret.getGithub();
         
-        var date = new Date();
-        date.seconds = date.getTime() / 1000;
-        date.offset = date.getTimezoneOffset();
+        $("#inputNameRefresh").click(this.generateName);
+        this.generateName();
         
-        //Commit the staged updates
-        repo.saveAs("commit", {
-          tree : treeHash,
-          author: {
-            name: "Dani Pletter",
-            email: "dani@everyside.com",
-            date: date
-          },
-          parent: headHash,
-          message: "Incremental Edit"
-        }, function(err, commitHash){
-          //Move dev branch to point at our new commit
-          repo.updateRef("refs/heads/"+branchName, commitHash, function(err, val){
-            console.log(val, err);
-            openShape(shapeName, userName, shapeRepoName);
-          });
-        //});
+        $("#inputName").keypress(this.updateRepoName);
+        $("#inputName").change(this.updateRepoName);
+        
+        $("#inputGithubToken").val(this.user.github.token);
+        $("#buttonAuthorize").click(cheddar.authorize);
+        $("#buttonCreate").click(cheddar.createShape);
+        
+        ret.fire("cheddar.init");
+        callback(val.githubToken);
       });
-    });
-  });
-}
-
-function openShape(shapeName, userName, repoName){
-  console.log("opening " + repoName);
-  var github = initGithub();
-  var repo = getRepo(userName+"/"+repoName);
-  
-  repo.readRef("refs/heads/"+"dev-"+userName, function(err, headHash){
-    repo.loadAs("commit", headHash, function(err, commit){
-      repo.loadAs("tree", commit.tree, function(err, tree){
-        var entry = tree[shapeName+".jscad"];
-        repo.loadAs("text", entry.hash, function(err, contents){
+    },
+    
+    openShape : function(shapeURL){
+      return null;
+    },
+    
+    getGithub : function(){
+      
+      var ret = new Github({
+        token: this.user.github.token,
+        auth: "oauth"
+      });
+      
+      var u = ret.getUser();
+      if(u){
+        console.log(u);
+        u.show(null,function(err, info) {
+          this.user.github.user = info.login;
           
-          chrome.app.window.create(
-            'viewer_jscad.html',
-            {
-              id:userName+"_"+repoName + "_viewer",
-              state:'normal',
-              'bounds': {
-                  'width': Math.round(window.screen.availWidth*0.4),
-                  'height': Math.round(window.screen.availHeight * 0.95),
-                  'left': Math.round(window.screen.availWidth*0.58)
-              },
-              frame:'none'
-            }, 
-            function(createdWindow) {
-              createdWindow.contentWindow.shapeCode = contents;
-            }
-          );
-          
-          chrome.app.window.create(
-            'editor_jscad.html',
-            {
-              id:userName+"_"+repoName + "_editor",
-              state:'normal',
-              'bounds': {
-                  'width': Math.round(window.screen.availWidth*0.4),
-                  'height': Math.round(window.screen.availHeight * 0.95),
-                  'left': Math.round(window.screen.availWidth*0.15)
-              },
-              frame:'none'
-            }, 
-            function(createdWindow) {
-              createdWindow.contentWindow.shapeCode = contents;
-              createdWindow.contentWindow.shapeViewer = userName+"_"+repoName + "_viewer"
-            }
-          );
-          
-          
+          this.fire("github.connect");
+          updateRepoName();
+        });
+        return ret;
+      }else{
+        return null;
+      }
+    },
+    
+    fire : function(){
+      
+    },
+    
+    handle : function(){
+      
+    },
+    
+    authorize : function(e){
+      this.user.github.token = this.val();
+      chrome.storage.sync.set({githubToken: this.user.github.token});
+      initGithub();
+    },
+      
+    generateName : function(){
+      var name = generate_name("shapes").toLowerCase();
+      $("#inputName").val(name);
+      updateRepoName();
+      return false;
+    },
+    
+    updateRepoName : function(){
+      chrome.storage.sync.get("repoPrefix", function(val){
+        var name = $("#inputName").val();
+        var prefix = val.repoPrefix || githubUser + "/cheddar-shape-";
+        var repoName = prefix + name;
+        $("#inputRepoName").val(repoName);
+      });
+    },
+    
+    createShape : function(){
+      var shapeName = $("#inputName").val();
+      var shapeDescription = $("#inputDescription").val();
+      var shapeRepoName = $("#inputRepoName").val();
+      var userName = githubUser;
+      if(shapeRepoName.indexOf("/") > -1){
+        var parts = shapeRepoName.split("/");
+        console.log(parts);
+        userName = parts[0];
+        shapeRepoName = parts[1];
+      }
+      
+      var fullRepoName = userName + "/" + shapeRepoName;
+      var github = initGithub();
+      console.log(github);
+      //fixme - support repo creation in orgs (if userName is an org).
+      github.getUser().createRepo({"name": shapeRepoName, auto_init : true}, function(err, res) {
+        console.log("created repo.", err);
+        
+        var githubRepo = github.getRepo(userName, shapeRepoName);
+        var branchName = "dev-"+githubUser;
+        githubRepo.branch(branchName, function(){
+          var repo = getRepo(userName+"/"+shapeRepoName);
+          repo.readRef("refs/heads/"+branchName, function(err, headHash){
+              
+            //repo.saveAs("blob", "Hello World\n", function(err, blobHash){
+              // Now we create a tree that is a folder containing the blob as `greeting.txt`
+              var filename = shapeName + ".jscad";
+              var updates = {
+                "shape.json" : {mode:modes.file, content:"{\n\n}"}
+              };
+              updates[filename] = {mode:modes.file, content:"function main(){return   CSG.roundedCube({radius: 10, roundradius: 2, resolution: 16}).union(CSG.sphere({radius:10, resolution: 16}).translate([5, 5, 5]));}"};
+              repo.createTree(updates, function(error, treeHash){
+                
+                var date = new Date();
+                date.seconds = date.getTime() / 1000;
+                date.offset = date.getTimezoneOffset();
+                
+                //Commit the staged updates
+                repo.saveAs("commit", {
+                  tree : treeHash,
+                  author: {
+                    name: "Dani Pletter",
+                    email: "dani@everyside.com",
+                    date: date
+                  },
+                  parent: headHash,
+                  message: "Change README.md to be all uppercase using js-github"
+                }, function(err, commitHash){
+                  //Move dev branch to point at our new commit
+                  repo.updateRef("refs/heads/"+branchName, commitHash, function(err, val){
+                    console.log(val, err);
+                    cheddar.openShape(fullRepoName);
+                  });
+                //});
+              });
+            });
+          });
         });
       });
-    });
-  });
-}
-
-$(function(){
-  
-  chrome.storage.sync.get("githubToken", function(val){
-    githubToken = val.githubToken;
-    $("#inputGithubToken").val(githubToken);
-    
-    initGithub();
-    
-    $("#inputNameRefresh").click(generateName);
-    generateName();
-    
-    $("#inputName").keypress(updateRepoName);
-    $("#inputName").change(updateRepoName);
-  });
-  
-  $("#buttonAuthorize").click(function(e){
-    githubToken = $("#inputGithubToken").val();
-    chrome.storage.sync.set({githubToken: githubToken});
-    initGithub();
-  });
-
-  $("#buttonCreate").click(function(){
-    
-    var shapeName = $("#inputName").val();
-    var shapeDescription = $("#inputDescription").val();
-    var shapeRepoName = $("#inputRepoName").val();
-    var userName = githubUser;
-    if(shapeRepoName.indexOf("/") > -1){
-      var parts = shapeRepoName.split("/");
-      console.log(parts);
-      userName = parts[0];
-      shapeRepoName = parts[1];
     }
+  };
+  
+  function ShapeController(shapeURL){
     
-    var fullRepoName = userName + "/" + shapeRepoName;
-    var github = initGithub();
-    console.log(github);
-    //fixme - support repo creation in orgs (if userName is an org).
-    github.getUser().createRepo({"name": shapeRepoName, auto_init : true}, function(err, res) {
-      console.log("created repo.", err);
+    var ret = {
       
-      var githubRepo = github.getRepo(userName, shapeRepoName);
-      var branchName = "dev-"+githubUser;
-      githubRepo.branch(branchName, function(){
+      editorView : null,
+      renderView : null,
+      repo : null,
+      
+      getGithub : function(){
+        
+      },
+      
+      save : function(repoName, code){
+        console.log("save!");
+        
         var repo = getRepo(userName+"/"+shapeRepoName);
         repo.readRef("refs/heads/"+branchName, function(err, headHash){
             
-          //repo.saveAs("blob", "Hello World\n", function(err, blobHash){
-            // Now we create a tree that is a folder containing the blob as `greeting.txt`
             var filename = shapeName + ".jscad";
             var updates = {
-              "shape.json" : {mode:modes.file, content:"{\n\n}"}
             };
-            updates[filename] = {mode:modes.file, content:"function main(){return   CSG.roundedCube({radius: 10, roundradius: 2, resolution: 16}).union(CSG.sphere({radius:10, resolution: 16}).translate([5, 5, 5]));}"};
+            updates[filename] = {mode:modes.file, content:code};
             repo.createTree(updates, function(error, treeHash){
               
               var date = new Date();
@@ -204,7 +189,7 @@ $(function(){
                   date: date
                 },
                 parent: headHash,
-                message: "Change README.md to be all uppercase using js-github"
+                message: "Incremental Edit"
               }, function(err, commitHash){
                 //Move dev branch to point at our new commit
                 repo.updateRef("refs/heads/"+branchName, commitHash, function(err, val){
@@ -215,60 +200,80 @@ $(function(){
             });
           });
         });
+      },
+      
+      view : function(shapeName, userName, repoName){
+        console.log("opening " + repoName);
+        var github = initGithub();
+        var repo = getRepo(userName+"/"+repoName);
         
-      });
-    });
-    
-    
-    return;
-    
-    chrome.storage.sync.get("githubToken", function(val){
-      repo.readRef("refs/heads/master", function(err, headHash){
-        repo.loadAs("commit", headHash, function(err, commit){
-          repo.loadAs("tree", commit.tree, function(err, tree){
-            
-            
-            var entry = tree["README.md"];
-            repo.loadAs("text", entry.hash, function(err, readme){
-              
-              var updates = [
-                {
-                  path: "README.md", // Update the existing entry
-                  mode: entry.mode,  // Preserve the mode (it might have been executible)
-                  content: readme.toUpperCase() // Write the new content
-                }
-              ];
-              
-              // Based on the existing tree, we only want to update, not replace.
-              updates.base = commit.tree;
-            
-              // Create the new file and the updated tree.
-              repo.createTree(updates, function(err, treeHash){
-              
-                var date = new Date();
-                date.seconds = date.getTime() / 1000;
-                date.offset = date.getTimezoneOffset();
+        repo.readRef("refs/heads/"+"dev-"+userName, function(err, headHash){
+          repo.loadAs("commit", headHash, function(err, commit){
+            repo.loadAs("tree", commit.tree, function(err, tree){
+              var entry = tree[shapeName+".jscad"];
+              repo.loadAs("text", entry.hash, function(err, contents){
                 
-                repo.saveAs("commit", {
-                  tree: treeHash,
-                  author: {
-                    name: "Dani Pletter",
-                    email: "dani@everyside.com",
-                    date: date
-                  },
-                  parent: headHash,
-                  message: "Change README.md to be all uppercase using js-github"
-                }, function(err, commitHash){
-          
-                  // Now we can browse to this commit by hash, but it's still not in master.
-                  // We need to update the ref to point to this new commit
-                  repo.updateRef("refs/heads/master", commitHash, function(){});
-                });
+                chrome.app.window.create(
+                  'viewer_jscad.html',
+                  {
+                    id:userName+"_"+repoName + "_viewer",
+                    state:'normal',
+                    'bounds': {
+                        'width': Math.round(window.screen.availWidth*0.4),
+                        'height': Math.round(window.screen.availHeight * 0.95),
+                        'left': Math.round(window.screen.availWidth*0.58)
+                    },
+                    frame:'none'
+                  }, 
+                  function(createdWindow) {
+                    createdWindow.contentWindow.shapeCode = contents;
+                  }
+                );
+                
+                chrome.app.window.create(
+                  'editor_jscad.html',
+                  {
+                    id:userName+"_"+repoName + "_editor",
+                    state:'normal',
+                    'bounds': {
+                        'width': Math.round(window.screen.availWidth*0.4),
+                        'height': Math.round(window.screen.availHeight * 0.95),
+                        'left': Math.round(window.screen.availWidth*0.15)
+                    },
+                    frame:'none'
+                  }, 
+                  function(createdWindow) {
+                    createdWindow.contentWindow.shapeCode = contents;
+                    createdWindow.contentWindow.shapeViewer = userName+"_"+repoName + "_viewer";
+                  }
+                );
               });
             });
           });
         });
-      });
-    });
-  });
+      }
+    };
+    return ret;
+  }
+  
+  return ret;
+}());
+
+
+
+
+
+window.addEventListener("message", function(event){
+  //var repoName = "FIXME";FIXME -- need to pass the shape metadata around.
+  //save(repoName, event.data);
+});
+
+
+
+
+
+$(function(){
+  
+  cheddar.init();
+  
 });
