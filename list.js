7a89c68b41fdd7c3b7a5cefcda5846f0e9298880
Dani Pletter <dani@fawnapp.com>
2015-07-24 11:44:46 -0700

    add indexeddb and in-memory caches for github

--- list.js
+++ list.js
@@ -165,6 +165,8 @@ var cheddar = (function CheddarController(){
       
       cheddar.generateName();
       cheddar.getShapeController({repo:shapeRepoName, name:shapeName, description: shapeDescription}, function(shapeController){
+        
+        $("#listTab > a").trigger("click");
         shapeController.create(function(){
           cheddar.updateList();
         });  
@@ -178,36 +180,37 @@ var cheddar = (function CheddarController(){
     var shapeRepoName = config.repo;
     
     var github = cheddar.getGithub();
-    var repo = getRepo(shapeRepoName);
+    getRepo(shapeRepoName, function(repo){
         
-    repo.readRef("refs/heads/"+"dev-"+cheddar.user.github.user, function(err, headHash){
-      if(headHash){
-        repo.loadAs("commit", headHash, function(err, commit){
-          if(commit){
-            repo.loadAs("tree", commit.tree, function(err, tree){
-              if(tree){
-                var entry = tree["shape.json"];
-                repo.loadAs("text", entry.hash, function(err, contents){
-                  if(contents){
-                    var shape = JSON.parse(contents);
-                    self.name = shape.name;
-                    self.description = shape.description;
-                    callback(self);
-                  }else{
-                    callback(self);
-                  }
-                });
-              }else{
-                callback(self);
-              }
-            });
-          }else{
-            callback(self);
-          }
-        });
-      }else{
-        callback(self);
-      }
+      repo.readRef("refs/heads/"+"dev-"+cheddar.user.github.user, function(err, headHash){
+        if(headHash){
+          repo.loadAs("commit", headHash, function(err, commit){
+            if(commit){
+              repo.loadAs("tree", commit.tree, function(err, tree){
+                if(tree){
+                  var entry = tree["shape.json"];
+                  repo.loadAs("text", entry.hash, function(err, contents){
+                    if(contents){
+                      var shape = JSON.parse(contents);
+                      self.name = shape.name;
+                      self.description = shape.description;
+                      callback(self);
+                    }else{
+                      callback(self);
+                    }
+                  });
+                }else{
+                  callback(self);
+                }
+              });
+            }else{
+              callback(self);
+            }
+          });
+        }else{
+          callback(self);
+        }
+      });
     });
     
     var self = {
@@ -245,18 +248,64 @@ var cheddar = (function CheddarController(){
           var githubRepo = github.getRepo(userName, shortRepoName);
           var branchName = "dev-"+cheddar.user.github.user;
           githubRepo.branch(branchName, function(){
-            var repo = getRepo(userName+"/"+shortRepoName);
-            repo.readRef("refs/heads/"+branchName, function(err, headHash){
-                
-              //repo.saveAs("blob", "Hello World\n", function(err, blobHash){
-                // Now we create a tree that is a folder containing the blob as `greeting.txt`
-                var filename = self.name + ".jscad";
-                var updates = {
-                  "shape.json" : {mode:modes.file, content:'{"name":"'+self.name+'","description":"'+self.description+'"}'}
-                };
-                updates[filename] = {mode:modes.file, content:"function main(){\n  return CSG.roundedCube({\n      radius: 10, \n      roundradius: 2, \n      resolution: 16\n    })\n    .union(\n    	CSG.sphere({\n          radius:10, \n         resolution: 16\n        })\n    	.translate([5, 5, 5])\n  	);\n}"};
-                repo.createTree(updates, function(error, treeHash){
+            getRepo(userName+"/"+shortRepoName, function(repo){
+              repo.readRef("refs/heads/"+branchName, function(err, headHash){
                   
+                //repo.saveAs("blob", "Hello World\n", function(err, blobHash){
+                  // Now we create a tree that is a folder containing the blob as `greeting.txt`
+                  var filename = self.name + ".jscad";
+                  var updates = {
+                    "shape.json" : {mode:modes.file, content:'{"name":"'+self.name+'","description":"'+self.description+'"}'}
+                  };
+                  updates[filename] = {mode:modes.file, content:"function main(){\n  return CSG.roundedCube({\n      radius: 10, \n      roundradius: 2, \n      resolution: 16\n    })\n    .union(\n    	CSG.sphere({\n          radius:10, \n         resolution: 16\n        })\n    	.translate([5, 5, 5])\n  	);\n}"};
+                  repo.createTree(updates, function(error, treeHash){
+                    
+                    var date = new Date();
+                    date.seconds = date.getTime() / 1000;
+                    date.offset = date.getTimezoneOffset();
+                    
+                    //Commit the staged updates
+                    repo.saveAs("commit", {
+                      tree : treeHash,
+                      author: {
+                        name: "Dani Pletter",
+                        email: "dani@everyside.com",
+                        date: date
+                      },
+                      parent: headHash,
+                      message: "Create Shape Repo"
+                    }, function(err, commitHash){
+                      //Move dev branch to point at our new commit
+                      repo.updateRef("refs/heads/"+branchName, commitHash, function(err, val){
+                        
+                        
+                        self.view();
+                        callback(self);
+                      });
+                    //});
+                  });
+                });
+              });
+            });
+          });
+        });
+      },
+      
+      update : function(code){
+        console.log("save!");
+        
+        getRepo(shapeRepoName, function(repo){
+          var branchName = "dev-"+cheddar.user.github.user;
+          repo.readRef("refs/heads/"+branchName, function(err, headHash){
+              repo.loadAs("commit", headHash, function(err, commit){
+                
+                var filename = self.name + ".jscad";
+                var updates = [
+                   {path:filename, mode:modes.file, content:code}
+                ];
+                updates.base = commit.tree;
+                repo.createTree(updates, function(error, treeHash){
+                
                   var date = new Date();
                   date.seconds = date.getTime() / 1000;
                   date.offset = date.getTimezoneOffset();
@@ -270,14 +319,12 @@ var cheddar = (function CheddarController(){
                       date: date
                     },
                     parent: headHash,
-                    message: "Create Shape Repo"
+                    message: "Incremental Edit"
                   }, function(err, commitHash){
                     //Move dev branch to point at our new commit
                     repo.updateRef("refs/heads/"+branchName, commitHash, function(err, val){
                       
-                      
                       self.view();
-                      callback(self);
                     });
                   //});
                 });
@@ -287,92 +334,53 @@ var cheddar = (function CheddarController(){
         });
       },
       
-      update : function(code){
-        console.log("save!");
-        
-        var repo = getRepo(shapeRepoName);
-        var branchName = "dev-"+cheddar.user.github.user;
-        repo.readRef("refs/heads/"+branchName, function(err, headHash){
-            repo.loadAs("commit", headHash, function(err, commit){
-              
-              var filename = self.name + ".jscad";
-              var updates = [
-                 {path:filename, mode:modes.file, content:code}
-              ];
-              updates.base = commit.tree;
-              repo.createTree(updates, function(error, treeHash){
-              
-                var date = new Date();
-                date.seconds = date.getTime() / 1000;
-                date.offset = date.getTimezoneOffset();
-                
-                //Commit the staged updates
-                repo.saveAs("commit", {
-                  tree : treeHash,
-                  author: {
-                    name: "Dani Pletter",
-                    email: "dani@everyside.com",
-                    date: date
-                  },
-                  parent: headHash,
-                  message: "Incremental Edit"
-                }, function(err, commitHash){
-                  //Move dev branch to point at our new commit
-                  repo.updateRef("refs/heads/"+branchName, commitHash, function(err, val){
-                    
-                    self.view();
-                  });
-                //});
-              });
-            });
-          });
-        });
-      },
-      
       view : function(){
         console.log("opening " + shapeRepoName);
         var github = cheddar.getGithub();
-        var repo = getRepo(shapeRepoName);
-        
-        repo.readRef("refs/heads/"+"dev-"+cheddar.user.github.user, function(err, headHash){
-          repo.loadAs("commit", headHash, function(err, commit){
-            repo.loadAs("tree", commit.tree, function(err, tree){
-              var entry = tree[self.name+".jscad"];
-              repo.loadAs("text", entry.hash, function(err, contents){
-                
-                chrome.app.window.create(
-                  'viewer_jscad.html',
-                  {
-                    id:cheddar.user.github.user+"_"+self.name + "_viewer",
-                    state:'normal',
-                    'bounds': {
-                        'width': Math.round(window.screen.availWidth*0.4),
-                        'height': Math.round(window.screen.availHeight * 0.95),
-                        'left': Math.round(window.screen.availWidth*0.58)
+        getRepo(shapeRepoName, function(repo){
+          
+          repo.readRef("refs/heads/"+"dev-"+cheddar.user.github.user, function(err, headHash){
+            repo.loadAs("commit", headHash, function(err, commit){
+              repo.loadAs("tree", commit.tree, function(err, tree){
+                var entry = tree[self.name+".jscad"];
+                repo.loadAs("text", entry.hash, function(err, contents){
+                  
+                  chrome.app.window.create(
+                    'viewer_jscad.html',
+                    {
+                      id:cheddar.user.github.user+"_"+self.name + "_viewer",
+                      state:'normal',
+                      focused:false,
+                      'bounds': {
+                          'width': Math.round(window.screen.availWidth*0.4),
+                          'height': Math.round(window.screen.availHeight * 0.95),
+                          'left': Math.round(window.screen.availWidth*0.58)
+                      }
+                    }, 
+                    function(createdWindow) {
+                      createdWindow.contentWindow.shapeCode = contents;
                     }
-                  }, 
-                  function(createdWindow) {
-                    createdWindow.contentWindow.shapeCode = contents;
-                  }
-                );
-                
-                chrome.app.window.create(
-                  'editor_jscad.html',
-                  {
-                    id:cheddar.user.github.user+"_"+self.name + "_editor",
-                    state:'normal',
-                    'bounds': {
-                        'width': Math.round(window.screen.availWidth*0.4),
-                        'height': Math.round(window.screen.availHeight * 0.95),
-                        'left': Math.round(window.screen.availWidth*0.15)
+                  );
+                  
+                  chrome.app.window.create(
+                    'editor_jscad.html',
+                    {
+                      id:cheddar.user.github.user+"_"+self.name + "_editor",
+                      state:'normal',
+                      focused:true,
+                      'bounds': {
+                          'width': Math.round(window.screen.availWidth*0.4),
+                          'height': Math.round(window.screen.availHeight * 0.95),
+                          'left': Math.round(window.screen.availWidth*0.15)
+                      }
+                    }, 
+                    function(createdWindow) {
+                      createdWindow.contentWindow.shapeCode = contents;
+                      createdWindow.contentWindow.shapeRepoName = shapeRepoName;
+                      createdWindow.contentWindow.shapeViewer = cheddar.user.github.user+"_"+self.name + "_viewer";
                     }
-                  }, 
-                  function(createdWindow) {
-                    createdWindow.contentWindow.shapeCode = contents;
-                    createdWindow.contentWindow.shapeRepoName = shapeRepoName;
-                    createdWindow.contentWindow.shapeViewer = cheddar.user.github.user+"_"+self.name + "_viewer";
-                  }
-                );
+                  );
+                });
               });
             });
           });
--- requirements.js
+++ requirements.js
@@ -1,12 +1,19 @@
-window.getRepo = function(repoName){
+window.getRepo = function(repoName, callback){
     
     var repo = {};
     require('js-github/mixins/github-db')(repo, repoName, cheddar.user.github.token);
     require('js-git/mixins/create-tree')(repo);
-    require('js-git/mixins/read-combiner')(repo);
-    require('js-git/mixins/formats')(repo);
-    window.modes = require('js-git/lib/modes');
-    return repo;
+    //indexedDB.deleteDatabase("tedit");
+    var indexD = require('js-git/mixins/indexed-db');
+    indexD.init(function(){
+      require('js-git/mixins/add-cache')(repo, indexD);
+      require('js-git/mixins/mem-cache')(repo);
+  
+      require('js-git/mixins/read-combiner')(repo);
+      require('js-git/mixins/formats')(repo);
+      window.modes = require('js-git/lib/modes');
+      callback(repo);
+    });
 };
 
 
